103afdf8cc7422aecd32f131315bf6391d0f55a9
João Gabriel <gama.gabrieljoao@gmail.com>
2025-12-03 15:55:50 -0300

    Matricula Finalizada

--- matriculas/urls.py
+++ matriculas/urls.py
@@ -5,6 +5,8 @@ app_name = 'matriculas'
 
 urlpatterns = [
     path('matriculas/', MatriculasAPIView.as_view(), name='matriculas'),
-    path('matriculas/<int:pk>/', MatriculaAPIView.as_view(), name='matricula'),
+    path('matriculas/<int:pk>/', MatriculaAPIView.as_view(), name='matricula'), ## Editar, deletar uma matricula especifica
+    path('alunos/<int:aluno_pk>/matriculas', MatriculasAPIView.as_view(), name='matriculas_por_aluno'),
+    path('alunos/<int:aluno_pk>/matriculas/<int:matricula_pk>/', MatriculaAPIView.as_view(), name='matricula_por_aluno'),
 ]
 
--- matriculas/views.py
+++ matriculas/views.py
@@ -1,11 +1,31 @@
 from rest_framework import generics
 from .models import Matricula
 from .serializers import MatriculaSerializer
+from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+
 
 class MatriculasAPIView(generics.ListCreateAPIView):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('aluno_pk'):
+            return self.queryset.filter(aluno_id=self.kwargs['aluno_pk'])
+        return self.queryset.all()
+
 class MatriculaAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
+    def get_object(self):
+        if self.kwargs.get('aluno_pk'):
+           return get_object_or_404(self.get_queryset(), aluno_id=self.kwargs['aluno_pk'], pk=self.kwargs.get('matricula_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('matricula_pk'))
+
+
+
+   
